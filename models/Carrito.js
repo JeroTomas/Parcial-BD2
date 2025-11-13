4e62e02a9dbcd9d8938ebd6d302192abe47cd168
JeroTomas <jerozu164@gmail.com>
2025-11-12 23:43:14 -0300

    Update Carrito.js

--- models/Carrito.js
+++ models/Carrito.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+// Cantidad de veces que seguarda el producto
+const itemSchema = new mongoose.Schema({
+  producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
+  cantidad: { type: Number, required: true, min: 1 }
+});
 
+// Verificacion del carrito y lista de productos en el carrito
 const carritoSchema = new mongoose.Schema({
-  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-  items: [
-    {
-      producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
-      cantidad: { type: Number, required: true, min: 1 },
-    },
-  ],
+  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', unique: true, required: true },
+  items: [itemSchema],
+  total: { type: Number, default: 0 } // se calcula al leer
 });
 
 module.exports = mongoose.model('Carrito', carritoSchema);
