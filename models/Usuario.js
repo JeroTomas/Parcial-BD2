346be16e4b2f8080ee549170d4625a7ca04758d3
JeroTomas <jerozu164@gmail.com>
2025-11-12 23:42:47 -0300

    Update Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -5,17 +5,20 @@ const usuarioSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  rol: { type: String, enum: ['cliente', 'admin'], default: 'cliente' },
-});
+  telefono: { type: String },
+  rol: { type: String, enum: ['admin', 'cliente'], default: 'cliente' }
+}, { timestamps: true });
 
+// Hash contraseña
 usuarioSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
+// Comparacion del hash con la contraseña
 usuarioSchema.methods.compararPassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
