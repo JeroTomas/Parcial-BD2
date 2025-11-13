3d26f5ebf927728fd58587f8ba4b93e605344569
JeroTomas <jerozu164@gmail.com>
2025-11-12 23:46:25 -0300

    Update Resena.js

--- models/Resena.js
+++ models/Resena.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
+// Detalles de la reseña
 const resenaSchema = new mongoose.Schema({
   usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
   producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
-  calificacion: { type: Number, required: true, min: 1, max: 5 },
   comentario: { type: String },
-  fecha: { type: Date, default: Date.now },
+  calificacion: { type: Number, required: true, min: 1, max: 10 },
+  fecha: { type: Date, default: Date.now }
 });
+
+// Una reseña por usuario/producto
+resenaSchema.index({ usuario: 1, producto: 1 }, { unique: true }); 
 
 module.exports = mongoose.model('Resena', resenaSchema);
