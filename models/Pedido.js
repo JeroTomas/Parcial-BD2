df4549584707d99a146ace49045abcfa82548db6
JeroTomas <jerozu164@gmail.com>
2025-11-12 23:45:28 -0300

    Update Pedido.js

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
+// Precio subtotal del producto
+const pedidoItemSchema = new mongoose.Schema({
+  producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
+  cantidad: { type: Number, required: true },
+  subtotal: { type: Number, required: true } // precioUnitario * cantidad guardado
+});
+
+// Detllaes del pedido
 const pedidoSchema = new mongoose.Schema({
   usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-  items: [
-    {
-      producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
-      cantidad: { type: Number, required: true },
-      precioUnitario: { type: Number, required: true },
-    },
-  ],
+  items: [pedidoItemSchema],
   total: { type: Number, required: true },
-  estado: { type: String, enum: ['pendiente', 'enviado', 'entregado'], default: 'pendiente' },
-  metodoPago: { type: String, required: true },
-  fecha: { type: Date, default: Date.now },
+  estado: { type: String, enum: ['pendiente','enviado','entregado','cancelado'], default: 'pendiente' },
+  metodoPago: { type: String, enum: ['tranferencia', 'efectivo', 'tarjeta'], required: true },
+  fecha: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Pedido', pedidoSchema);
